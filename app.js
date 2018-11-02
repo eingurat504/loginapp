e0519a3eeffe3341378e695b8cfc8e653dc352a6
eingurat <e.ingurat@bsmart-technology.com>
2018-11-02 10:00:46 +0300

    error handling implementation

--- app.js
+++ app.js
@@ -85,7 +85,7 @@ app.use(function(req, res, next){
 });
 
 app.use('/', routes);
-app.use('users', users);
+app.use('/users', users);
 
 //Set Port
 app.set('port', (process.env.PORT || 3000));
--- routes/users.js
+++ routes/users.js
@@ -11,5 +11,33 @@ router.get('/login', function(req, res){
     res.render('login');
 });
 
+//Get Register page
+router.post('/register', function(req, res){
+    
+        var name = req.body.name;
+        var email = req.body.email;
+        var username = req.body.username;
+        var password = req.body.password;
+        var confirm_password = req.body.confirm_password;
+console.log(name);
+        //validation implementation
+        req.checkBody('name', 'Name is required').notEmpty();
+        req.checkBody('email', 'Email is required').notEmpty();
+        req.checkBody('email', 'Email is not valid').isEmail();
+        req.checkBody('username', 'Username is required').notEmpty();
+        req.checkBody('password', 'Password is required').notEmpty();
+        req.checkBody('confirm_password', 'Password does not match').equals(req.body.password);
+
+        var errors = req.validationErrors();
+        if(errors){
+            res.render('register',{
+                errors:errors
+            });
+        }else{
+            console.log('PASSED');
+        }
+
+});
+
 
 module.exports = router;
